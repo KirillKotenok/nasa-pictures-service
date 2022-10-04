dd3744167d5e9ec75ddae268e54b9d8a56873ab5
Kirill Kotenok <kirill.kotenok@data-ox.com>
2022-10-04 17:46:31 +0300

    Initial commit

---
+++ src/test/java/com/example/nasapic/NasaPicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nasapic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NasaPicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
